033a86174d5325aefb107a986279bcb663c25d43
akbar masud <akbarmasud@akbars-MBP.home>
2018-01-24 17:36:11 -0500

    hi

---
+++ src/test/java/JenkinsDemo.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+
+	@Test
+	public void main() {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("hi");
+
+	}
+
+}
